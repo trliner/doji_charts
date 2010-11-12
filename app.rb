a4017f0229087ccaaeb8fb2a293ea03d11c62555
Tim <trliner@gmail.com>
2010-11-12 01:52:25 -0600

    pass date range in through params

--- app.rb
+++ app.rb
@@ -7,8 +7,9 @@ Bundler.require(:default)
 
 require 'app/base'
 
-get '/' do
-  range = StockQuote.range("2010-10-1", "2010-10-28")
+# /2010-10-22/2010-10-28
+get '/:start_date/:end_date' do
+  range = StockQuote.range(params[:start_date], params[:end_date])
   @page_title = "Doji Charts"
   @chart_title = StockQuote.chart_title(range)
   @data = StockQuote.chart_data(:candle, range)
